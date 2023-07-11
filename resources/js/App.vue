505dd0f9e3ceee3943c43c2530e68de049acb5ea
Lapinas <andrejus.sablovinskis@addendumgroup.com>
2023-07-11 10:32:19 +0300

    initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    setup() {
+
+    }
+}
+</script>
+
+<script setup>
+  import {ref} from "vue";
+
+  const name = ref('Jonas')
+</script>
+
+<template>
+  <h1> Hello, VueJs3 with Laravel 10 ! {{name}}</h1>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import fs from 'fs';
+import path from 'path';
+
+export default defineConfig({
+    server: {
+        host: 'localhost', // replace with your domain
+        https: {
+            key: fs.readFileSync(path.join(__dirname, 'data/certificates/localhost.key')),
+            cert: fs.readFileSync(path.join(__dirname, 'data/certificates/localhost.crt')),
+        },
+        port: 8000,
+        open: true
+        // other options...
+    },
+    plugins: [
+		vue(),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+	resolve: {
+		alias: {
+			vue: "vue/dist/vue.esm-bundler.js",
+		}
+	}
+});
