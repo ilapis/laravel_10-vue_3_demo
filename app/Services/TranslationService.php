c89f9c3b20f446eb5bd51dad8cdcf46def8d09e4
lapinas <sablo.andre@gmail.com>
2023-08-05 13:47:56 +0300

    translation cache

--- app/Services/TranslationService.php
+++ app/Services/TranslationService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Translation;
 use App\Data\TranslationData;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class TranslationService
@@ -18,34 +19,51 @@ class TranslationService
 
     public function locale(string $language_code): array
     {
-        return Translation::query()
-            ->whereHas('language', function ($query) use ($language_code) {
-                $query->where('code', $language_code);
-            })
-            ->get(['group', 'key', 'value'])
-            ->groupBy('group')
-            ->map(function ($groupItems) {
-                return $groupItems->pluck('value', 'key');
-            })
-            ->toArray();;
+        return Cache::rememberForever('locale-' . $language_code, function () use ($language_code) {
+            return Translation::query()
+                ->whereHas('language', function ($query) use ($language_code) {
+                    $query->where('code', $language_code);
+                })
+                ->get(['group', 'key', 'value'])
+                ->groupBy('group')
+                ->map(function ($groupItems) {
+                    return $groupItems->pluck('value', 'key');
+                })
+                ->toArray();
+        });
     }
 
     public function create(TranslationData $dto): Translation
     {
-        return Translation::create($dto->toArray());
+        $translation = Translation::create($dto->toArray());
+
+        $this->invalidateCache($translation->language->code);
+
+        return $translation->fresh();
     }
 
     public function update(Translation $translation, TranslationData $dto): ?Translation
     {
         $translation->update($dto->toArray());
 
+        $this->invalidateCache($translation->language->code);
+
         return $translation->fresh();
     }
 
     public function delete(Translation $translation): ?Translation
     {
+        $languageCode = $translation->language->code;
+
         $translation->delete();
 
+        $this->invalidateCache($languageCode);
+
         return $translation->fresh();
+    }
+
+    private function invalidateCache(string $language_code): void
+    {
+        Cache::forget('locale-' . $language_code);
     }
 }
